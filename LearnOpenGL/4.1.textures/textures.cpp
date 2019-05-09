8c467fc9585944bef6ba54bc44ad4eabadbab334
Ikaruga <ahjo_bb@hotmail.com>
2019-05-09 19:06:54 +0800

    update

--- LearnOpenGL/4.1.textures/textures.cpp
+++ LearnOpenGL/4.1.textures/textures.cpp
@@ -3,9 +3,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
+#include <stb_image.h>
 
-#include "shader_s.h"
+#include <learnopengl/shader_s.h>
 
 #include <iostream>
 
